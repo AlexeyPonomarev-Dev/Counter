d2b738e9e01f2c4dc0a1ebb2bbee92019ba1ab3c
Alexey Ponomarev <alexey.ponomarev.dev@gmail.com>
2023-03-16 09:54:50 +0300

    Сделано домашнее задание, спринт 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum ChangeString: String {
+    case change = "Значение измененно на"
+    case reset = "Значение сброшенно"
+    case negativeDecrement = "попытка уменьшить значение счётчика ниже 0"
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var resetCounterButton: UIButton!
+    @IBOutlet weak var decrementButton: UIButton!
+    @IBOutlet weak var incrementButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        historyTextView.layer.borderWidth = 1
+        historyTextView.layer.cornerRadius = 12
+        counterLabel.text = "0"
+    }
+
+    @IBAction func decrement(_ sender: Any) {
+        if (counterLabel.text == "0") {
+            setHistory(with: .negativeDecrement)
+
+            return;
+        }
+        
+        guard let text = counterLabel.text, let count = Int(text)  else { return }
+        
+        counterLabel.text = String(count - 1)
+        setHistory(with: .change, and: "-1")
+
+    }
+    
+    @IBAction func increment(_ sender: Any) {
+        guard let text = counterLabel.text, let count = Int(text)  else { return }
+        
+        counterLabel.text = String(count + 1)
+        
+        setHistory(with: .change, and: "+1")
+    }
+    
+    @IBAction func resetCount(_ sender: Any) {
+        counterLabel.text = "0"
+        
+        setHistory(with: .reset)
+    }
+}
+
+// MARK: private methods
+extension ViewController {
+    private func getCurrentTimeString() -> String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    private func setHistory(with string: ChangeString, and value: String? = nil)  {
+        guard let historyText = historyTextView.text else { return }
+        let time = getCurrentTimeString()
+    
+        
+        historyTextView.text = "\(historyText) \n [\(time)]: \(string.rawValue) \(value ?? "")"
+    }
+}
